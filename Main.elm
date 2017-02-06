17f66851dd75a08e4de74281b3aea24d579b73be
Martin Troxler <marmutro@gmail.com>
2017-02-06 18:35:53 +0100

    Initial commit

---
+++ Main.elm
@@ -0,0 +1,108 @@
+port module Owntracks exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { url : String
+    , locations : List LocationInfo
+    }
+
+
+type alias LocationInfo =
+    { topic : String
+    , info : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" [], Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UrlChange String
+    | Connect
+    | LocationUpdate LocationInfo
+
+
+type alias Location =
+    { ltype : String
+    , acc : Int
+    , alt : Int
+    , batt : Int
+    , cog : Int
+    , desc : String
+    , event : String
+    , lat : Float
+    , lon : Float
+    , rad : Int
+    , trig : String
+    , tid : String
+    , tst : Int
+    , vac : Int
+    , vel : Int
+    , p : Int
+    , conn : String
+    }
+
+
+port connect : String -> Cmd msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange newUrl ->
+            ( Model newUrl [], Cmd.none )
+
+        Connect ->
+            ( model, connect model.url )
+
+        LocationUpdate newLocation ->
+            ( Model model.url (model.locations ++ [ newLocation ]), Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+port locationUpdate : (LocationInfo -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    locationUpdate LocationUpdate
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ onInput UrlChange ] []
+        , button [ onClick Connect ] [ text "Connect" ]
+        , div [] (List.map (\info -> div [] [ text (info.topic ++ info.info) ]) model.locations)
+        ]
